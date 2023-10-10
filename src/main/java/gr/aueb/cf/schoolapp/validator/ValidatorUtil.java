f3659f1b45e5d5533e1352f5639d4ace5e3c9d6b
filippos dimitriadis <fildimitriadis1998@gmail.com>
2023-10-10 13:16:24 +0300

    initialize project

---
+++ src/main/java/gr/aueb/cf/schoolapp/validator/ValidatorUtil.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.schoolapp.validator;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ValidatorUtil {
+    private static final Validator validator;
+
+    static {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    public static <T> List<String> validateDTO(T dto) {
+        Set<ConstraintViolation<T>> violations = validator.validate(dto);
+        List<String> errors = new ArrayList<>();
+        if (!violations.isEmpty()) {
+            for (ConstraintViolation<T> violation : violations) {
+                errors.add(violation.getMessage());
+            }
+        }
+        return errors;
+    }
+}
